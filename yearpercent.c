7be050fa2905f90e112f7fa8fc61c142e2a6f96a
moth <90016996+mothbrained@users.noreply.github.com>
2025-02-25 14:20:34 -0600

    Micro Optimization
    These make less of a performance difference than people on stack overflow will have you believe, but they are improvements nevertheless.

--- yearpercent.c
+++ yearpercent.c
@@ -3,24 +3,21 @@
 
 int main(){
     ///// CUSTOMIZE 
-    int progressbar_size = 50; // how many hashtags should the progress bar have in total?
+    const int progressbar_size = 50; // how many hashtags should the progress bar have in total?
     ///// CUSTOMIZE END
 
-    time_t seconds = time(NULL);
-    int year = seconds / 31556926 + 1970;
-    float percentage = (float)(seconds % 31556926) / 31556926;
+    const time_t seconds = time(NULL);
+    const float percentage = (float)(seconds % 31556926) / 31556926;
 
-    printf("We are %d%% of the way through %d\n[", (int)(percentage * 100), year);
-
+    printf("We are %d%% of the way through %d\n[\x1B[32m", (int)(percentage * 100), seconds / 31556926 + 1970);
     for (size_t i = 0; i < (int)(percentage * progressbar_size); i++){
-        printf("\x1B[32m#");
+        printf("#");
     }
-    printf("\x1B[31m");
+    printf("\x1B[31m"); // red
     for (size_t i = 0; i < progressbar_size - (int)(percentage * progressbar_size); i++){
         printf("-");
     }
-    
-    printf("\033[0m]\n");
+    printf("\033[0m]\n"); // none
     
     return 0;
 }
